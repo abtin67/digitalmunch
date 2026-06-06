d2954e5bdbfee857e311f4c57870b6ecda843938
abtin67 <andishmandabtin8@gmail.com>
2026-06-06 20:30:07 +0330

    responsiv AdminItemPage

--- app/[locale]/admin/page.tsx
+++ app/[locale]/admin/page.tsx
@@ -6,10 +6,34 @@ export default function AdminDashboardPage() {
       <p className="text-gray-400 mt-2">خوش آمدید! از منوی سایدبار برای مدیریت استفاده کنید.</p>
       
       {/* اینجا بعداً نمودارها یا خلاصه وضعیت سفارش‌ها رو اضافه می‌کنیم */}
-      <div className="mt-8 grid grid-cols-3 gap-6">
+      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         <div className="bg-[#160924] p-6 rounded-2xl border border-white/10">
           <h3 className="text-gray-400">تعداد محصولات</h3>
-          <p className="text-4xl font-black text-amber-500 mt-2">۲۶</p>
+          <p className="text-4xl font-black text-amber-500 mt-2">+۴۸</p>
+        </div>
+        <div className="bg-[#160924] p-6 rounded-2xl border border-white/10">
+          <h3 className="text-gray-400">تعداد کمبو</h3>
+          <p className="text-4xl font-black text-amber-500 mt-2">۴</p>
+        </div>
+        <div className="bg-[#160924] p-6 rounded-2xl border border-white/10">
+          <h3 className="text-gray-400">تعداد پیتزا</h3>
+          <p className="text-4xl font-black text-amber-500 mt-2">۲۱</p>
+        </div>
+        <div className="bg-[#160924] p-6 rounded-2xl border border-white/10">
+          <h3 className="text-gray-400">تعداد پاستا</h3>
+          <p className="text-4xl font-black text-amber-500 mt-2">۴</p>
+        </div>
+        <div className="bg-[#160924] p-6 rounded-2xl border border-white/10">
+          <h3 className="text-gray-400">تعداد ساندویچ</h3>
+          <p className="text-4xl font-black text-amber-500 mt-2">۱۰</p>
+        </div>
+        <div className="bg-[#160924] p-6 rounded-2xl border border-white/10">
+          <h3 className="text-gray-400">تعداد برگر</h3>
+          <p className="text-4xl font-black text-amber-500 mt-2">۶</p>
+        </div>
+        <div className="bg-[#160924] p-6 rounded-2xl border border-white/10">
+          <h3 className="text-gray-400">تعداد پیش غذا</h3>
+          <p className="text-4xl font-black text-amber-500 mt-2">۷</p>
         </div>
       </div>
     </div>
